90a827b6389dee9695a3b529656cd54f7047dad5
camilevahviraki <camilevahviraki@gmail.com>
2022-04-05 00:16:11 +0200

    Fix linters errors in index.js file

--- index.js
+++ index.js
@@ -1,85 +1,62 @@
-    function showNav() {
-        var nav_Id = document.getElementById("My_navList");
+function showNav() {
+  const UnionIcon = document.getElementById('UnionIcon');
+  const navClass = document.getElementsByClassName('nav_list');
+  const pngList = document.getElementsByClassName('links_png');
+  const logo1 = document.querySelector('a');
+  const NavAll = document.querySelector('nav');
+  const section12 = document.getElementsByClassName('p_section12');
+  const hideNav1 = document.getElementById('clickMe2');
+  const showNav1 = document.getElementById('clickMe1');
+  const navClassIl = document.querySelectorAll('.nav_menu');
 
-        var UnionIcon = document.getElementById("UnionIcon");
+  UnionIcon.style.display = 'none';
+  logo1.style.display = 'none';
+  NavAll.style.height = '100vh';
+  NavAll.style.alignItems = 'baseline';
+  hideNav1.style.display = 'flex';
+  showNav1.style.display = 'none';
+  hideNav1.style.top = '12px';
 
-        let nav_class = document.getElementsByClassName('nav_list');
+  for (let i = 0; i < navClass.length; i += 1) {
+    navClass[i].style.display = 'flex';
+    navClass[i].style.padding = '2px 0 0 0';
+    section12[i].style.margin = '100vh 0 0 0';
+    pngList[i].style.margin = '6px 0 120px 0';
+  }
 
-        let png_list = document.getElementsByClassName('links_png');
+  for (let a = 0; a < navClassIl.length; a += 1) {
+    navClassIl[a].style.padding = '16px';
+  }
 
-        let logo_1 = document.querySelector('a');
+  console.log('NavBar available');
+}
 
-        let Nav_all = document.querySelector('nav');
+function hideNav() {
+  const NavAll = document.querySelector('nav');
+  const UnionIcon = document.getElementById('UnionIcon');
+  const logo = document.querySelector('a');
+  const navClass = document.getElementsByClassName('nav_list');
+  const pngList = document.getElementsByClassName('links_png');
+  const section12 = document.getElementsByClassName('p_section12');
+  const hideNav = document.getElementById('clickMe2');
+  const showNav = document.getElementById('clickMe1');
 
-        let nav_class_il = document.querySelectorAll('.nav_menu');
+  UnionIcon.style.display = 'none';
+  logo.style.display = 'flex';
+  NavAll.style.alignItems = 'center';
+  NavAll.style.height = '48px';
+  hideNav.style.display = 'none';
+  showNav.style.display = 'flex';
 
-        let li_nav_ul = document.querySelectorAll('.li_nav');
+  for (let i = 0; i < navClass.length; i += 1) {
+    navClass[i].style.padding = 'calc((100vh - 184px) / 2) 0 0 0';
+    section12[i].style.margin = '48px 0 0 0';
+    pngList[i].style.margin = '6px 0 6px 0';
+    navClass[i].style.display = 'none';
+  }
 
-        let section12 = document.getElementsByClassName('p_section12');
+  console.log('NavBar Hiden');
+}
 
-        let hide_nav = document.getElementById('clickMe2');
-
-        let show_nav = document.getElementById('clickMe1');
-
-        UnionIcon.style.display = 'none';
-        logo_1.style.display = 'none';
-        Nav_all.style.height = '100vh';
-        Nav_all.style.alignItems = 'baseline';
-        hide_nav.style.display = 'flex';
-        show_nav.style.display = 'none';
-        hide_nav.style.top = '12px';
-
-        for (var i = 0; i < nav_class.length; i++) {
-            nav_class[i].style.display = 'flex';
-            nav_class[i].style.padding = '2px 0 0 0';
-            section12[i].style.margin = '100vh 0 0 0';
-            png_list[i].style.margin = '6px 0 120px 0';
-        }
-
-        for (var a = 0; a < nav_class_il.length; a++) {
-            nav_class_il[a].style.padding = '16px';
-        }
-
-        console.log("NavBar available");
-    }
-
-    function hideNav() {
-        let Nav_all = document.querySelector('nav');
-
-        var UnionIcon = document.getElementById("UnionIcon");
-
-        let logo = document.querySelector('a');
-
-        let nav_class = document.getElementsByClassName('nav_list');
-
-        let png_list = document.getElementsByClassName('links_png');
-
-        let nav_class_il = document.querySelectorAll('.nav_menu');
-
-        let li_nav_ul = document.querySelectorAll('.li_nav');
-
-        let section12 = document.getElementsByClassName('p_section12');
-
-        let hide_nav = document.getElementById('clickMe2');
-
-        let show_nav = document.getElementById('clickMe1');
-
-        UnionIcon.style.display = 'none';
-        logo.style.display = 'flex';
-        Nav_all.style.alignItems = 'center';
-        Nav_all.style.height = '48px';
-        hide_nav.style.display = 'none';
-        show_nav.style.display = 'flex';
-
-
-        for (var i = 0; i < nav_class.length; i++) {
-
-            nav_class[i].style.padding = 'calc((100vh - 184px) / 2) 0 0 0';
-            section12[i].style.margin = '48px 0 0 0';
-            png_list[i].style.margin = '6px 0 6px 0';
-            nav_class[i].style.display = 'none';
-        }
-
-        console.log("NavBar Hiden");
-    }
-    console.log("Phone screen")
+showNav();
+hideNav();
